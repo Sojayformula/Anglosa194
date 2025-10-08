178a245eecb866f085ea0cf4d880dca56e092266
emma <formula865@yahoo.com>
2025-10-08 16:11:37 +0000

    updated

--- src/app/pages/constitution/constitution-layout/constitution-layout.component.ts
+++ src/app/pages/constitution/constitution-layout/constitution-layout.component.ts
@@ -10,4 +10,25 @@ import { RouterModule } from '@angular/router';
 })
 export class ConstitutionLayoutComponent {   
 
+
+  
+   sideOpen = false;
+  pages = [1,2,3,4,5,6,7,8]; // example pages; replace with your data
+
+   isOpen = false;
+   isHovering = false;
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
+
+   openSidebar() {
+    this.isOpen = true;
+  }
+
+  closeSidebar() {
+    this.isOpen = false;
+  }
+
+
 }
